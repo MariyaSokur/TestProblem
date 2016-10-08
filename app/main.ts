a4f2ae251c4d0061916c04146419303439c95c2c
Sokyr NA <psihirulat@i.ua>
2016-10-08 23:25:30 +0300

    Added:styles, removing of tasks

--- app/main.ts
+++ app/main.ts
@@ -6,23 +6,33 @@ import {bootstrap} from 'angular2/platform/browser';
 import {Task} from './task';
 
 @Component({
-    selector:'todo-app',
+    selector: 'todo-app',
     templateUrl: './app/main.html',
-    styleUrls: ['./app/main.css']
+    styleUrls: ['app/main.css']
 })
 
-class AppCompoment{
-        tasks: Task[]=[];
+class AppCompoment {
+    tasks: Task[] = [];
 
-        constructor(){
-            this.tasks.push(new Task('1','desc1',false));
-            this.tasks.push(new Task('2' ,'desc2',true));
-            this.tasks.push(new Task('3','desc3',false));
-        }
+    constructor() {
+        this.tasks.push(new Task('1', 'desc1', false));
+        this.tasks.push(new Task('3rsfgfdffffff fffff fffffff fffffffffff ffffff ffffffff', 'fffffffffff fffffff fffffffffffff ffffffffff ffffff ffffffff ffffffffff', false));
+        this.tasks.push(new Task('2', 'desc2', true));
+        this.tasks.push(new Task('3', 'desc3', false));
+    }
 
-        add(title:string,description:string,checked:boolean){
-            this.tasks.push(new Task(title,description,checked));
+    add(title: string, description: string, checked: boolean) {
+        this.tasks.push(new Task(title, description, checked));
+    }
+
+    delete(task: Task) {
+        if(task){
+            let index = this.tasks.indexOf(task);
+            if(index>-1) {
+                this.tasks.splice(index,1);
+            }
         }
+    }
 }
 
 bootstrap(AppCompoment);
